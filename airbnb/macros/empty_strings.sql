cb4e47a6cc7bf700bfc0c6c42b30294818888bcd
Matt Cadel <matthew.cadel@mobysoft.com>
2026-04-19 14:21:58 +0100

    updates following progression in course

--- airbnb/macros/empty_strings.sql
+++ airbnb/macros/empty_strings.sql
@@ -1,7 +1,7 @@
-{% macro no_empty_strings(model) %}
+{% macro empty_strings(model) %}
     {%- for col in adapter.get_columns_in_relation(model) -%}
         {%- if col.is_string() %}
-            {{ col.name }} IS NOT NULL AND {{ col.name }} <> '' AND
+            {{ col.name }} IS NULL OR {{ col.name }} = '' AND
         {%- endif %}
     {%- endfor %}
     TRUE
